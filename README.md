dd9fdb2ab5c0b13f1e5fe3b00c3a8d384a668882
Alexey Samovich <alexey.samovich@gmail.com>
2024-01-11 03:59:23 +0100

    init: init project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Video Chat with WebRTC and Firebase
+WebRTC project highly inspirated by https://github.com/fireship-io/webrtc-firebase-demo.
+
+## Usage
+To run locally:
+```
+npm install
+
+npm run dev
+```
+
+To deploy on Heroku:
+
+```
+heroku init
+
+git add .
+git commit -m "Init project"
+git push heroku main
+```
+
+## Sources 
+https://www.agora.io/en/blog/how-does-webrtc-work/
+https://developer.liveswitch.io/liveswitch-server/guides/what-are-stun-turn-and-ice.html
+https://github.com/fireship-io/webrtc-firebase-demo
---
+++ main.js
@@ -0,0 +1,213 @@
+const firebaseConfig = {
+  // firebase config
+};
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+const firestore = firebase.firestore();
+
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+// Global State
+let pc = new RTCPeerConnection(servers);
+let localStream = null;
+let remoteStream = null;
+
+// HTML elements
+const webcamVideo = document.getElementById('webcamVideo');
+const callButton = document.getElementById('callButton');
+const callInput = document.getElementById('callInput');
+const answerButton = document.getElementById('answerButton');
+const remoteVideo = document.getElementById('remoteVideo');
+const hangupButton = document.getElementById('hangupButton');
+const copiedSuccessState = document.getElementById('copiedSuccessState');
+const copiedFailedState = document.getElementById('copiedFailedState');
+
+// 1. Setup media sources
+window.onload = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  remoteStream = new MediaStream();
+
+  // Push tracks from local stream to peer connection
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  webcamVideo.srcObject = localStream;
+  webcamVideo.muted = true;
+  remoteVideo.srcObject = remoteStream;
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+};
+
+// 2. Create an offer
+callButton.onclick = async () => {
+  // Reference Firestore collections for signaling
+  const callDoc = firestore.collection('calls').doc();
+  const offerCandidates = callDoc.collection('offerCandidates');
+  const answerCandidates = callDoc.collection('answerCandidates');
+
+  navigator.clipboard.writeText(callDoc.id).then(() => {
+    copiedSuccessState.style.visibility = "visible";
+  }, () => {
+    return;
+  });
+
+  // Get candidates for caller, save to db
+  pc.onicecandidate = (event) => {
+    event.candidate && offerCandidates.add(event.candidate.toJSON());
+  };
+
+  // Create offer
+  const offerDescription = await pc.createOffer();
+  await pc.setLocalDescription(offerDescription);
+
+  const offer = {
+    sdp: offerDescription.sdp,
+    type: offerDescription.type,
+  };
+
+  await callDoc.set({ offer });
+
+  // Listen for remote answer
+  callDoc.onSnapshot((snapshot) => {
+    const data = snapshot.data();
+    if (!pc.currentRemoteDescription && data?.answer) {
+      const answerDescription = new RTCSessionDescription(data.answer);
+      pc.setRemoteDescription(answerDescription);
+    }
+  });
+
+  // When answered, add candidate to peer connection
+  answerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'added') {
+        const candidate = new RTCIceCandidate(change.doc.data());
+        pc.addIceCandidate(candidate);
+      }
+    });
+  });
+};
+
+// 3. Answer the call with the unique ID
+answerButton.onclick = async () => {
+  const callId = callInput.value;
+  const callDoc = firestore.collection('calls').doc(callId);
+  const answerCandidates = callDoc.collection('answerCandidates');
+  const offerCandidates = callDoc.collection('offerCandidates');
+
+  pc.onicecandidate = (event) => {
+    event.candidate && answerCandidates.add(event.candidate.toJSON());
+  };
+
+  const callData = (await callDoc.get()).data();
+
+  const offerDescription = callData.offer;
+  await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+  const answerDescription = await pc.createAnswer();
+  await pc.setLocalDescription(answerDescription);
+
+  const answer = {
+    type: answerDescription.type,
+    sdp: answerDescription.sdp,
+  };
+
+  await callDoc.update({ answer });
+
+  offerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      console.log(change);
+      if (change.type === 'added') {
+        let data = change.doc.data();
+        pc.addIceCandidate(new RTCIceCandidate(data));
+      }
+    });
+  });
+
+  hangupButton.disabled = false;
+};
+
+pc.onconnectionstatechange = (ev) => {
+  switch (pc.connectionState) {
+    case "closed":
+    case "disconnected":
+    case "failed":
+      pc.close();
+      remoteStream = new MediaStream();
+
+      pc = new RTCPeerConnection(servers);
+      
+      localStream.getTracks().forEach((track) => {
+        pc.addTrack(track, localStream);
+      });
+
+      pc.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteStream.addTrack(track);
+        });
+      };
+
+      remoteVideo.srcObject = remoteStream;
+
+      callInput.value = "";
+
+      copiedSuccessState.style.visibility = "hidden";
+
+      callButton.disabled = false;
+      answerButton.disabled = false;
+      hangupButton.disabled = true;
+      break;
+
+    case "connected":
+      hangupButton.disabled = false;
+    default:
+      break;
+  }
+};
+
+
+hangupButton.onclick = async () => {
+  pc.close();
+  remoteStream = new MediaStream();
+
+  pc = new RTCPeerConnection(servers);
+  
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+
+  // Pull tracks from remote stream, add to video stream
+  pc.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  remoteVideo.srcObject = remoteStream;
+
+  callInput.value = "";
+
+  copiedSuccessState.style.visibility = "hidden";
+
+  callButton.disabled = false;
+  answerButton.disabled = false;
+  hangupButton.disabled = true;
+}
